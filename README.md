1af04368064e21e4a33444f8d14b4c93048416bc
Barış Soner Uşaklı <barisusakli@gmail.com>
2015-03-11 14:06:51 -0400

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# nodebb-plugin-category-notifications
+
+Allows user to subscribe to categories and get notifications whenever a new topic or post is made.
+
---
+++ public/scripts/client.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function() {
+
+	$(window).on('action:ajaxify.end', function(ev, data) {
+		if (app.template === 'category') {
+			//add button
+		}
+	});
+
+
+});
