46446fdeb72a8c343332a6c3c08dd2f4af288abb
barisusakli <barisusakli@gmail.com>
2016-12-23 11:41:54 +0300

    closes #4 closes #5

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,38 +1,44 @@
+'use strict';
 
+/* globals app, socket, ajaxify*/
 
 $(document).ready(function() {
 
-	$(window).on('action:ajaxify.end', function(ev, data) {
+	$(window).on('action:ajaxify.end', function() {
+
 		if (app.template === 'category' && app.user.uid) {
-			var unsubscribeHtml = '<button type="button" class="btn btn-default btn-warning unsubscribe"><i class="fa fa-pencil"></i> Unsubscribe</button>';
-			var subscribeHtml = '<button type="button" class="btn btn-default btn-success subscribe"><i class="fa fa-pencil"></i> Subscribe</button>';
+			var unsubscribeHtml = '<button type="button" class="btn btn-default btn-warning unsubscribe"><i class="fa fa-pencil"></i> [[categorynotifications:unsubscribe]]</button>';
+			var subscribeHtml = '<button type="button" class="btn btn-default btn-success subscribe"><i class="fa fa-pencil"></i> [[categorynotifications:subscribe]]</button>';
 
 			var cid = ajaxify.data.cid;
-			socket.emit('plugins.categoryNotifications.isSubscribed', {cid: cid}, function(err, isSubscribed) {
-				if (err) {
-					return app.alertError(err.message);
-				}
+			require(['translator'], function (translator) {
+				socket.emit('plugins.categoryNotifications.isSubscribed', {cid: cid}, function(err, isSubscribed) {
 
-				var btn = isSubscribed ? $(unsubscribeHtml) : $(subscribeHtml);
+					function handleClick(className, method) {
+						$('.category').on('click', className, function() {
+							socket.emit(method, {cid: cid}, function(err) {
+								if (err) {
+									return app.alertError(err.message);
+								}
+								var btn = className === '.subscribe' ? unsubscribeHtml : subscribeHtml;
+								translator.translate(btn, function(translated) {
+									$(className).replaceWith($(translated));
+								});
+							});
+						});
+					}
 
-				$('[component="category/controls"]').prepend(btn);
+					if (err) {
+						return app.alertError(err.message);
+					}
 
-				$('.category').on('click', '.subscribe', function() {
-					socket.emit('plugins.categoryNotifications.subscribe', {cid: cid}, function(err) {
-						if (err) {
-							return app.alertError(err.message);
-						}
-						$('.subscribe').replaceWith($(unsubscribeHtml));
+					var btn = isSubscribed ? unsubscribeHtml : subscribeHtml;
+					translator.translate(btn, function (translated) {
+						$('[component="category/controls"]').prepend($(translated));
 					});
-				});
 
-				$('.category').on('click', '.unsubscribe', function() {
-					socket.emit('plugins.categoryNotifications.unsubscribe', {cid: cid}, function(err) {
-						if (err) {
-							return app.alertError(err.message);
-						}
-						$('.unsubscribe').replaceWith($(subscribeHtml));
-					});
+					handleClick('.subscribe', 'plugins.categoryNotifications.subscribe');
+					handleClick('.unsubscribe', 'plugins.categoryNotifications.unsubscribe');
 				});
 			});
 		}
