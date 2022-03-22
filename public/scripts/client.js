dcd6c4be965e3f5f588874987a7447c9ce655a3a
Barış Soner Uşaklı <barisusakli@gmail.com>
2022-03-22 15:11:40 -0400

    lint: client side

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,24 +1,22 @@
 'use strict';
 
-/* globals app, socket, ajaxify*/
-
-$(document).ready(function() {
-	$(window).on('action:ajaxify.end', function() {
+$(document).ready(function () {
+	$(window).on('action:ajaxify.end', function () {
 		if (ajaxify.data.template.category && app.user.uid) {
 			var unsubscribeHtml = '<button type="button" class="btn btn-default btn-warning unsubscribe"><i class="fa fa-pencil"></i> [[categorynotifications:unsubscribe]]</button>';
 			var subscribeHtml = '<button type="button" class="btn btn-default btn-success subscribe"><i class="fa fa-pencil"></i> [[categorynotifications:subscribe]]</button>';
 
 			var cid = ajaxify.data.cid;
 			require(['translator', 'alerts'], function (translator, alerts) {
-				socket.emit('plugins.categoryNotifications.isSubscribed', {cid: cid}, function(err, isSubscribed) {
+				socket.emit('plugins.categoryNotifications.isSubscribed', { cid: cid }, function (err, isSubscribed) {
 					function handleClick(className, method) {
-						$('.category').on('click', className, function() {
-							socket.emit(method, {cid: cid}, function(err) {
+						$('.category').on('click', className, function () {
+							socket.emit(method, { cid: cid }, function (err) {
 								if (err) {
 									return alerts.error(err.message);
 								}
 								var btn = className === '.subscribe' ? unsubscribeHtml : subscribeHtml;
-								translator.translate(btn, function(translated) {
+								translator.translate(btn, function (translated) {
 									$(className).replaceWith($(translated));
 								});
 							});
